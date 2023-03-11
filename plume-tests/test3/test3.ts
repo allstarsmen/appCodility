d4997ec0e03530713431440f8e6a681f9b611a30
Nam Pham <allstarsmen@gmail.com>
2023-03-11 13:48:07 +0700

    add solution

--- plume-tests/test3/test3.ts
+++ plume-tests/test3/test3.ts
@@ -1,1 +1,29 @@
+export class Task {
+    solution(A: number[]): number {
+        let count = 0;
+        
+        const obj: any = {}
+        A.forEach(no => {
+            if (obj[no] == null) {
+                obj[no] = 1
+            } else {
+                obj[no]++
+            }
+        });
+        
+        const seen: any = {}
+        Object.values(obj).forEach(val => {
+            let no = val as number
+            while(seen[''+no] != null && no > 0) { 
+                no--
+                count++
+            }
+
+            seen[''+no] = 1
+        });
+        
+
+        return count
+    } 
+}
 
